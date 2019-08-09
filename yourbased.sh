9a879b289d21cf0f013bcd0ee9dd73b82f5457aa
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-09 00:11:26 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -2,10 +2,8 @@
 set -ex
 export DEBIAN_FRONTEND=noninteractive
 apt-get update && apt-get install -y tzdata
-gem install bundler -v 2.0.1
 # before_install
 gem update --system
-gem uninstall bundler -x
 gem install --force bundler -v 1.17.3
 # install
 bundle install --jobs=3 --retry=3
