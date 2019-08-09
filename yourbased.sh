74605a34cc504d3d0a607a89d8a29a52263e04e3
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-09 00:02:58 -0400

    Create yourbased.sh

---
+++ yourbased.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y tzdata
+gem install bundler -v 2.0.1
+# before_install
+rvm use @global
+gem update --system
+gem uninstall bundler -x
+gem install --force bundler -v 1.17.3
+# install
+bundle install --jobs=3 --retry=3
+# before_script
+bundle exec rake db:migrate
+# script
+rake
